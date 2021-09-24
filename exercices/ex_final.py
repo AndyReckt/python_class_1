a64f38f9011b516267a0e1c7590a443c76e1d738
AndyReckt <alex123aze@gmail.com>
2021-09-24 22:41:33 +0200

    finished the final exercise, hopefully will get a good grade!

--- exercices/ex_final.py
+++ exercices/ex_final.py
@@ -1,3 +1,6 @@
+import random
+import sys
+
 pkms = (
     {"name": "Marowak", "points": 3, "good": False},
     {"name": "Weezing", "points": 2, "good": False},
@@ -20,12 +23,17 @@ pkms = (
     {"name": "Rotom", "points": 2, "good": True},
     {"name": "Politoed", "points": 4, "good": True},
 )  # tuple that contains all the available Pokemon to fight
+starters = (
+    {"life": 10, "penalty": 3, "bonus": 1, "name": "Bulbasaur"},
+    {"life": 15, "penalty": 2, "bonus": 2, "name": "Charmander"},
+    {"life": 20, "penalty": 1, "bonus": 3, "name": "Charizard"}
+)  # tuple that contains all the available starters
 
 
-def chose_difficulty():  # function that lets u choose the difficulty of the game
+def choose_difficulty():  # function that lets u choose the difficulty of the game
     while True:
         diff = input("Choose ur difficulty: (0 for easy, 1 for hard, 2 for challenge) ")
-        if diff.lower() == 0 or diff.lower() == "easy" or diff.lower() == "e":
+        if diff.lower() == '0' or diff.lower() == "easy" or diff.lower() == "e":
             return 0
         elif diff.lower() == '1' or diff.lower() == "hard" or diff.lower() == "h":
             return 1
@@ -39,11 +47,11 @@ def chose_starter():  # function that lets u choose your starter for the game
     while True:
         start = input("Choose ur starter: (b for Bulbasaur, c for Charmander, ch for Charizard) ")
         if start.lower() == '0' or start.lower() == "Bulbasaur" or start.lower() == "b":
-            return {"life": 10, "penalty": 3, "bonus": 1, "name": "Bulbasaur"}
+            return starters[0]
         elif start.lower() == '1' or start.lower() == "Charmander" or start.lower() == "c":
-            return {"life": 15, "penalty": 2, "bonus": 2, "name": "Charmander"}
+            return starters[1]
         elif start.lower() == '2' or start.lower() == "Charizard" or start.lower() == "ch":
-            return {"life": 20, "penalty": 1, "bonus": 3, "name": "Charizard"}
+            return starters[2]
         else:
             print("Sorry but i cant understand this value...")
 
@@ -60,7 +68,70 @@ def custom_name(pk):  # function that lets u choose a custom name for your pokem
             print("Sorry but i cant understand this value...")
 
 
-difficulty = chose_difficulty()  # choosing difficulty
+def encounter_pokemon(pokemon, round):
+    wild = pkms[random.randint(0, len(pkms) - 1)]
+    print(f"You encountered a {wild['name']}")
+
+    if starter['ran'] == False:
+        run = input("Do you want to run? (y/n)")
+        while True:
+            if run.lower() == 'y' or run.lower() == 'yes' or run.lower() == 'o' or run.lower() == 'oui' or run.lower() == 'x':
+                starter['ran'] = True
+                return
+            elif run.lower() == 'n' or run.lower() == 'no' or run.lower() == 'non':
+                break
+            else:
+                print("Sorry but i cant understand this value...")
+    else:
+        print(f"Since you ran last encounter, you will now loose {2 + wild['points']} life points")
+        pokemon['life'] -= 2 + wild['points']
+        starter['ran'] = False
+
+    if wild['good']:
+        print(f"You encountered a {wild['name']} which is a good pokemon, {pokemon['name']} will now gain {wild['points']} life points")
+        pokemon['life'] += wild['points']
+        print(f"{pokemon['name']} now has {pokemon['life']} life points.")
+    elif pokemon['life'] <= wild['points']:
+        print(f"You encountered a {wild['name']} which is a bad pokemon, {pokemon['name']} will now lose {wild['points']} life points")
+        print(f"Unfortunately, {pokemon['name']} only has {pokemon['life']} life points, and died :(")
+        sys.exit(f"You lost in {round} rounds")
+    else:
+        print(f"You encountered a {wild['name']} which is a bad pokemon, {pokemon['name']} will now lose {wild['points']} life points")
+        pokemon['life'] -= wild['points']
+        print(f"{pokemon['name']} now has {pokemon['life']} life points.")
+
+
+def left_right():
+    r = input("Which door do you want to open? (left/right)")
+    while True:
+        if r.lower() == 'l' or r.lower() == 'left' or r.lower() == 'g' or r.lower() == 'gauche':
+            print("You chose the left door...")
+            break
+        elif r.lower() == 'r' or r.lower() == 'right' or r.lower() == 'd' or r.lower() == 'droite':
+            print("You chose the right door...")
+            break
+        else:
+            print("Sorry but i cant understand this value...")
+
+
+def game(diff, pokemon):  # the main game function, that will run the game
+    i = 0
+    if diff == 0:
+        rounds = 4
+    elif diff == 1:
+        rounds = 11
+    else:
+        rounds = 19
+    while i <= rounds:
+        i += 1
+        left_right()
+        encounter_pokemon(pokemon, i)
+    sys.exit(f"Wow, you won!! {pokemon['name']} had {pokemon['life']} life points left. Congrats!!")
+
+
+difficulty = choose_difficulty()  # choosing difficulty
 starter = chose_starter()  # choosing starter
 starter['name'] = custom_name(starter.get("name"))   # choosing starter name
+starter['ran'] = False
 
+game(difficulty, starter)  # starting the game
